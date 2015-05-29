f9b1ae659404aa098f14444bdec2cb32235b0163
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-05-28 23:11:55 -0300

    adjusting todo example

--- components/root/root.ts
+++ components/root/root.ts
@@ -1,20 +1,20 @@
 /// <reference path="../../typings/angular2/angular2.d.ts" />
+
 import {Component, View} from 'angular2/angular2';
 import {Todo} from "components/todo/todo";
 
 @Component({
   selector: 'root'
 })
-
 @View({
   templateUrl: "components/root/root.tpl.html",
   directives: [ Todo ]
 })
-
 export class Root {
-  title: string;
+  title:string;
+  t: Todo;
   constructor() {
-    this.title = "Introduction to Angular2";
+    this.title = "Todo";
   }
 }
 
--- components/todo/todo.ts
+++ components/todo/todo.ts
@@ -1,12 +1,43 @@
 /// <reference path="../../typings/angular2/angular2.d.ts" />
 
-import {Component, View} from 'angular2/angular2';
+import {Component, View, EventEmitter} from 'angular2/angular2';
+import {NgFor} from 'angular2/directives';
+import {Validators, formDirectives, ControlGroup, FormBuilder} from 'angular2/forms';
 
-@Component({selector: 'todo'})
-@View({templateUrl: 'components/todo/todo.tpl.html'})
+@Component({
+    selector: 'todo'
+})
+@View({
+    templateUrl: 'components/todo/todo.tpl.html',
+    directives: [NgFor, formDirectives]
+})
 export class Todo {
     title: string;
+    todos: any[];
+    todoForm: ControlGroup;
+    secretWordChecker: EventEmitter;
+
     constructor() {
+        var _fb = new FormBuilder();
+
         this.title = 'Todo';
+        this.todos = [];
+        this.secretWordChecker = new EventEmitter();
+
+        this.todoForm = _fb.group({
+            "message": ["abc", Validators.required]
+        });
+    }
+
+    add(message:string) {
+        this.todos.push({value: message, id: Date.now()});
+    }
+
+    remove(id:number) {
+        this.todos.forEach((t, i) =>
+        {
+            if (id === t.id)
+                this.todos.splice(1, i);
+        });
     }
 }
