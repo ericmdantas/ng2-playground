f7c08a60010c6ae089ddd1fd8648e18805534a5e
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-06-02 11:46:09 -0300

    ....

--- common/services/todo.service.ts
+++ common/services/todo.service.ts
@@ -3,11 +3,11 @@
 import {Promise} from 'angular2/src/facade/async';
 
 export class TodoService {
-    add(value:string) {
+    add(value:string):Promise {
         return new Promise((res, rej) => res(value));
     }
 
-    remove(id:number) {
+    remove(id:number):Promise {
         return new Promise((res, rej) => res(id));
     }
 }
