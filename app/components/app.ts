a1ac8e70e2a322292c88dc6529ec2dbc57ad78e0
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-07-12 23:28:27 -0300

    route working

--- app/components/app.ts
+++ app/components/app.ts
@@ -1,57 +1,19 @@
-import {Component, View, NgFor, NgIf} from 'angular2/angular2';
-import {Inject} from 'angular2/di';
-import {FormBuilder, Validators, ControlGroup, formDirectives} from 'angular2/forms';
-import {AppService} from './app_service';
-import {Stuff} from './stuff_type';
-import {TrashCan} from '../common/trash_can';
-import {Card} from '../common/card';
+import {Component, View} from 'angular2/angular2';
+import {RouteConfig, RouterOutlet} from 'angular2/router';
+import {Ng2} from './ng2';
 
 @Component({
-  selector: 'app',
-  viewInjector: [FormBuilder, AppService]
+  selector: 'app'
 })
 @View({
-    templateUrl: 'app/components/app.html',
-    directives: [formDirectives, NgIf, Card, TrashCan],
-    styleUrls: ['app/components/app.css']
+    template: `<router-outlet></router-outlet>`,
+    directives: [RouterOutlet]
 })
+@RouteConfig([
+    {path: '/', redirectTo: '/ng2'},
+    {path: '/ng2', component: Ng2}
+])
 
 export class App {
-    stuffList: List<Stuff>;
-    stuffForm: ControlGroup;
-    service: AppService;
-    stuffCount: int = 0;
-    subTitle: string = `There are this much stuff: `;
 
-    constructor(@Inject(FormBuilder) fb: FormBuilder, appService: AppService) {
-        this.stuffForm = fb.group({
-            "info": ["something", Validators.required]
-        });
-
-        this.service = appService;
-        this.stuffList = [];
-    }
-
-    add():void {
-        this.service
-            .add(this.stuffForm.value.info)
-            .subscribe(res => {
-                this.stuffList.push({info: res, createdAt: Date.now()});
-                this.stuffCount++;
-        });
-    }
-
-    remove(date: Date):void {
-        this.service
-            .remove(this.stuffList, date)
-            .subscribe(list => {
-               this.stuffList = list;
-               if (this.stuffCount)
-                   this.stuffCount--;
-            });
-    }
-
-    somethingDropped(date: Date) {
-        this.remove(date);
-    }
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,5 @@
 import {bootstrap} from 'angular2/angular2';
 import {routerInjectables} from 'angular2/router';
 import {App} from 'app/components/app';
-import {NavigationApp} from 'app/components/navigation_app';
 
 bootstrap(App, [routerInjectables]);
-bootstrap(NavigationApp, [routerInjectables]);
