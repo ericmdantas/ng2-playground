f1e9c9b7cd243a16b7475290788a9796a6cbcae9
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-07-13 07:55:43 -0300

    .;.;.,

--- app/components/app.ts
+++ app/components/app.ts
@@ -1,17 +1,21 @@
 import {Component, View} from 'angular2/angular2';
-import {RouteConfig, RouterOutlet} from 'angular2/router';
+import {RouteConfig, RouterOutlet, RouterLink} from 'angular2/router';
 import {Ng2} from './ng2';
+import {Main} from './main';
 
 @Component({
   selector: 'app'
 })
 @View({
-    template: `<router-outlet></router-outlet>`,
-    directives: [RouterOutlet]
+    template: `
+    <a router-link="ng2">go to ng2</a>
+    <router-outlet></router-outlet>
+    `,
+    directives: [RouterOutlet, RouterLink]
 })
 @RouteConfig([
-    {path: '/', redirectTo: '/ng2'},
-    {path: '/ng2', component: Ng2}
+    {path: '/', component: Main},
+    {path: '/ng2', component: Ng2, as: 'ng2'}
 ])
 
 export class App {
---
+++ app/components/main.ts
@@ -0,0 +1,14 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+    selector: 'main-stuff'
+})
+@View({
+    template: '<p>main-stuff</p>'
+})
+
+export class Main {
+
+}
