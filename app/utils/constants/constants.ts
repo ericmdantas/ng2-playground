748633207e920b8911ec5497cc9c6ac5e2b9e788
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-08-03 08:02:39 -0300

    .;.;..;

--- app/utils/constants/constants.ts
+++ app/utils/constants/constants.ts
@@ -1,3 +1,6 @@
 export const FIGHT_STARTED = 'fight_started_event';
 export const FIGHT_ENDED = 'fight_ended_event';
 export const SOMEONE_GOT_HIT = 'someone_got_hit';
+export const PLAYER_GOT_HIT = 'player_got_hit';
+export const MONSTER_GOT_HIT = 'monster_got_hit';
+
--- app/world/stats/stats_cmp.ts
+++ app/world/stats/stats_cmp.ts
@@ -1,20 +1,40 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
 import {Component, View} from 'angular2/angular2';
+import {NgFor} from 'angular2/directives';
 import {StatsModel} from 'app/world/stats/stats_model';
+import {MessageBus, PLAYER_GOT_HIT, MONSTER_GOT_HIT} from 'app/utils/utils';
 
 @Component({
     selector: 'stats'
 })
 @View({
     templateUrl: 'app/world/stats/stats.html',
-    styleUrls: ['app/world/stats/stats.css']
+    styleUrls: ['app/world/stats/stats.css'],
+    directives: [NgFor]
 })
 
 export class StatsCmp {
     title: string = 'Stats';
+    totalHitsDelivered: number[] = [];
+    totalHitsReceived: number[] = [];
+    maxHitReceived: number = 0;
+    maxHitDelivered: number = 0;
+    totalHitsReceived: number[] = [];
+    playerDeaths: number = 0;
+    monsterDeaths: number = 0;
+    monsterKilled: number = 0;
+    monsterDeaths: number = 0;
+    mb: MessageBus = MessageBus;
 
     constructor() {
         console.log('stats_cmp init');
+
+        this.mb.listen(PLAYER_GOT_HIT, this.updateStuff.bind(this));
+        this.mb.listen(MONSTER_GOT_HIT, this.updateStuff.bind(this));
+    }
+
+    updateStuff(info: Object):void {
+
     }
 }
