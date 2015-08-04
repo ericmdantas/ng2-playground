096504eeafe55334ba6120522775241b8c0c2a6a
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-08-03 23:44:19 -0300

    .;.;.;.;

---
+++ app/world/battle/battle_actions/ressurrect_cmp.ts
@@ -0,0 +1,14 @@
+/// <reference path="../../../../typings/tsd.d.ts" />
+
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+    selector: 'ressurrect'
+})
+@View({
+    template: `<h1></h1>`
+})
+
+export class RessurrectCmp {
+
+}
