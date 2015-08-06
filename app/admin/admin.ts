9362f08f9b5903722fb502702d8f85b48ad566d9
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-08-06 07:57:30 -0300

    .;.;

--- app/admin/admin.ts
+++ app/admin/admin.ts
@@ -1,3 +1,3 @@
-export * from './admin_cmp';
-export * from './admin_service';
 export * from './admin_model';
+export * from './admin_service';
+export * from './admin_cmp';
---
+++ app/admin/admin_cmp.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import {Component, View} from 'angular2/angular2';
+import {AdminModel} from 'app/admin/admin_model';
+
+@Component({
+    selector: 'admin'
+})
+@View({
+    templateUrl: 'app/admin/admin.html',
+    styleUrls: ['app/admin/admin.css']
+})
+
+export class AdminCmp {
+
+}
