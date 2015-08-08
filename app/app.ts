87a64246112a6da4a3f4c85c70c364241cddf63c
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-08-08 00:20:59 -0300

    .;.;.

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,50 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-import {Component, View} from 'angular2/angular2';
+import {Component, View, Directive, ElementRef, EventEmitter, LifecycleEvent} from 'angular2/angular2';
 import {Inject} from 'angular2/di';
 import {Xtorage} from 'app/utils/xtorage';
 
+@Directive({
+    selector: '[checker]',
+    host: {
+        '(even)': 'onEven()',
+        '(odd)': 'onOdd()',
+        '(^click)': 'wut()'
+    },
+    hostInjector: [LifecycleEvent.onInit]
+})
+
+class CheckerDirective {
+    el: ElementRef;
+
+    constructor(@Inject(ElementRef) el: ElementRef) {
+        this.el = el;
+    }
+
+    onEven() {
+        console.log('even something')
+    }
+
+    onOdd() {
+        console.log('odd something')
+    }
+
+    wut() {
+        console.log('wut');
+    }
+
+    onInit() {
+        console.log('directive init')
+    }
+}
+
 @Component({
-    selector: 'app'
+    selector: 'app',
+    events: ['even', 'odd']
 })
 @View({
     template: `
+    <div checker>
         <h1>app</h1>
         <button type="button"
                 (click)="something()">smh</button>
@@ -16,6 +52,9 @@ import {Xtorage} from 'app/utils/xtorage';
         <p [inner-text]="counter"
            [class.something-even]="isEven()"
            [class.something-odd]="isOdd()"></p>
+
+        <strong id="new"></strong>
+    </div>
     `,
     styles: [`
         .something-even {
@@ -25,21 +64,34 @@ import {Xtorage} from 'app/utils/xtorage';
         .something-odd {
             color: blue;
         }
-    `]
+    `],
+    directives: [CheckerDirective]
 })
 export class AppCmp {
     counter: number = 0;
     storage: Xtorage = new Xtorage();
+    even: EventEmitter = new EventEmitter();
+    odd: EventEmitter = new EventEmitter();
 
     constructor() {
     }
 
     isEven() {
-        return this.counter % 2 !== 0;
+        if (this.counter % 2 !== 0) {
+            this.even.next({});
+            return true;
+        }
+
+        return false;
     }
 
     isOdd() {
-        return this.counter % 2 === 0;
+        if (this.counter % 2 === 0) {
+            this.odd.next({});
+            return true;
+        }
+
+        return false;
     }
 
     something() {
