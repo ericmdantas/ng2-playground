ef583c6b6d6e3d3f09d8b62a44dbffb4bfb0316b
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-12-04 11:55:00 -0200

    .;.;

--- app/main.ts
+++ app/main.ts
@@ -12,6 +12,7 @@ import {
 import {AppCmp} from 'app/app.js';
 import {Router1Cmp} from 'app/routes/route1.js';
 import {Router2Cmp} from 'app/routes/route2.js';
+import {Router3Cmp} from 'app/routes/route3.js';
 
 @Component({
   selector: 'main-cmp',
@@ -19,6 +20,7 @@ import {Router2Cmp} from 'app/routes/route2.js';
     <a [router-link]="['/App']">app</a>
     <a [router-link]="['/Router1', {id: r1.id, something: r1.something}]">route1</a>
     <a [router-link]="['/Router2', {yo: r2.yo}]">route2</a>
+    <a [router-link]="['/Router3', {yo: r2.yo, whatever: 'si'}]">route3</a>
     <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES]
@@ -26,7 +28,8 @@ import {Router2Cmp} from 'app/routes/route2.js';
 @RouteConfig([
   {path: '/', component: AppCmp, name: 'App'},
   {path: '/route1/:id', component: Router1Cmp, name: 'Router1'},
-  {path: '/route2/:yo', component: Router2Cmp, name: 'Router2'}
+  {path: '/route2/:yo', component: Router2Cmp, name: 'Router2'},
+  {path: '/route3/hey', component: Router3Cmp, name: 'Router3'}
 ])
 export class MainCmp implements OnInit {
   r1: Object = {id: 1, something: 'abc'};
---
+++ app/routes/route3.ts
@@ -0,0 +1,13 @@
+import {
+  Component
+} from 'angular2/angular2';
+
+@Component({
+  selector: 'router3',
+  template: `
+    <h1>router 3</h1>
+  `
+})
+export class Router3Cmp {
+
+}
