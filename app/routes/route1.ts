b9d85d0d0a0c606f10c318bd0ce36e45f5c32ab8
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-12-07 13:19:23 -0200

    .;.;

--- app/routes/route1.ts
+++ app/routes/route1.ts
@@ -3,14 +3,31 @@ import {
   OnDestroy
 } from 'angular2/angular2';
 
+import {
+  CanActivate,
+  CanDeactivate
+} from 'angular2/router';
+
 @Component({
   selector: 'router1',
   template: `
     <h1>router 1</h1>
   `
 })
-export class Router1Cmp implements OnDestroy {
+@CanActivate(() => {
+  return true;
+})
+export class Router1Cmp implements OnDestroy, CanDeactivate {
   ngOnDestroy() {
     console.log('destroying route1');
+  }
+
+  routerOnActivate() {
+    console.log('activating...');
+  }
+
+  routerCanDeactivate(n, p) {
+    console.log('deactivating..');
+    console.log(n, p);
   }
 }
--- app/routes/route2.ts
+++ app/routes/route2.ts
@@ -3,11 +3,18 @@ import {
   OnDestroy
 } from 'angular2/angular2';
 
+import {
+  CanActivate
+} from 'angular2/router';
+
 @Component({
   selector: 'router2',
   template: `
     <h1>router 2</h1>
   `
+})
+@CanActivate(() => {
+  return true; // false
 })
 export class Router2Cmp implements OnDestroy {
   ngOnDestroy() {
