4230ed4db2f5bd5d9b5cf5ebd04811785a44b2d9
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-12-10 19:48:42 -0200

    .

--- app/app.ts
+++ app/app.ts
@@ -6,11 +6,9 @@ import {
 import {MainContainerCmp} from './main_container_cmp/main_container_cmp.js';
 import {SubContainerCmp} from './sub_container_cmp/sub_container_cmp.js';
 import {CustomFormCmp} from './custom_form_cmp/custom_form_cmp.js';
-import {CustomHttpCmp} from './custom_http_cmp/custom_http_cmp.js';
 import {CustomAnnotationCmp} from './custom_annotation_cmp/custom_annotation_cmp.js';
 import {CustomAnnotationMethodCmp} from './custom_annotation_cmp/custom_annotation_method_cmp.js';
 import {MainBubbleEventsCmp} from './bubble_events_cmp/bubble_events_cmp.js';
-import {FormHttpCmp} from './form_http_cmp/form_http_cmp.js';
 import {SimpleCmp} from './simple_directive/simple_directive.js';
 
 @Component({
@@ -19,14 +17,6 @@ import {SimpleCmp} from './simple_directive/simple_directive.js';
     <main-container>
       <sub-container-cmp>
         <main-bubble-event></main-bubble-event>
-      </sub-container-cmp>
-
-      <sub-container-cmp>
-        <form-http-cmp></form-http-cmp>
-      </sub-container-cmp>
-
-      <sub-container-cmp>
-        <custom-http-cmp></custom-http-cmp>
       </sub-container-cmp>
 
       <sub-container-cmp>
@@ -51,9 +41,8 @@ import {SimpleCmp} from './simple_directive/simple_directive.js';
 
     </main-container>
   `,
-  directives: [MainContainerCmp, CustomFormCmp, CustomHttpCmp,
-               CustomAnnotationCmp, CustomAnnotationMethodCmp, SubContainerCmp,
-               MainBubbleEventsCmp, FormHttpCmp, SimpleCmp]
+  directives: [MainContainerCmp, CustomFormCmp, CustomAnnotationCmp,
+               CustomAnnotationMethodCmp, SubContainerCmp, MainBubbleEventsCmp, SimpleCmp]
 })
 export class AppCmp {
 
