d8eb3ee42e6cfebc451459ec6e7a079c760966da
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-12-14 22:14:42 -0200

    .

--- index.ts
+++
@@ -1,8 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {HTTP_PROVIDERS} from 'angular2/http';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {MainCmp} from 'app/main.js';
-
-bootstrap(MainCmp, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
-  .then(() => console.log('app init'))
-  .catch((err) => console.log(err));
