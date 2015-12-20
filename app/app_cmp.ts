05d24898d4ac8d330ded0e3b426ee8ca9e4c10a2
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-12-20 21:46:35 -0200

    .

--- app/app_cmp.ts
+++ app/app_cmp.ts
@@ -6,6 +6,7 @@ import {ContainerCmp} from './container_cmp/container_cmp.js';
 import {FormCmp} from './form_cmp/form_cmp.js';
 import {CmpWithOutput} from './cmp_with_output/cmp_with_output.js';
 import {CmpWithInput} from './cmp_with_input/cmp_with_input.js';
+import {CmpWithHttp} from './cmp_with_http/cmp_with_http.js';
 
 @Component({
   selector: 'app-cmp',
@@ -21,8 +22,12 @@ import {CmpWithInput} from './cmp_with_input/cmp_with_input.js';
     <container-cmp>
       <cmp-with-input [inIn]="'yoYoYo'"></cmp-with-input>
     </container-cmp>
+
+    <container-cmp>
+      <cmp-with-http></cmp-with-http>
+    </container-cmp>
   `,
-  directives: [ContainerCmp, FormCmp, CmpWithOutput, CmpWithInput]
+  directives: [ContainerCmp, FormCmp, CmpWithOutput, CmpWithInput, CmpWithHttp]
 })
 export class AppCmp {
   name: string = 'app';
