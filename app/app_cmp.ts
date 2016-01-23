d287ceabcd505f25edddcce2e74b17512b865e70
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-01-22 22:09:34 -0200

    and here we go again

--- app/app_cmp.ts
+++ app/app_cmp.ts
@@ -1,39 +1,11 @@
-import {
-  Component
-} from 'angular2/core';
-
-import {ContainerCmp} from './container_cmp/container_cmp.js';
-import {FormCmp} from './form_cmp/form_cmp.js';
-import {CmpWithOutput} from './cmp_with_output/cmp_with_output.js';
-import {CmpWithInput} from './cmp_with_input/cmp_with_input.js';
-import {CmpWithHttp} from './cmp_with_http/cmp_with_http.js';
-import {CmpWithCustomPipe} from './cmp_with_custom_pipe/cmp_with_custom_pipe.js';
+import {Component} from 'angular2/core';
 
 @Component({
-  selector: 'app-cmp',
+  selector: 'my-app',
   template: `
-    <container-cmp>
-      <form-cmp></form-cmp>
-    </container-cmp>
-
-    <container-cmp>
-      <cmp-with-output></cmp-with-output>
-    </container-cmp>
-
-    <container-cmp>
-      <cmp-with-input [inIn]="'yoYoYo'"></cmp-with-input>
-    </container-cmp>
-
-    <container-cmp>
-      <cmp-with-http></cmp-with-http>
-    </container-cmp>
-
-    <container-cmp>
-      <cmp-with-custom-pipe></cmp-with-custom-pipe>
-    </container-cmp>
-  `,
-  directives: [ContainerCmp, FormCmp, CmpWithOutput, CmpWithInput, CmpWithHttp, CmpWithCustomPipe]
+    <h1>{{name}}</h1>
+  `
 })
-export class AppCmp {
-  name: string = 'app';
+export class App {
+  name: string = 'yo!';
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {HTTP_PROVIDERS} from 'angular2/http';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {MainCmp} from './app/main.js';
+import {App} from './app/app_cmp.js';
 
-bootstrap(MainCmp, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
+bootstrap(App);
